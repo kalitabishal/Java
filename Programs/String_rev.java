f1c88951baabf032e9705340c23cd7e23d6a5db2
kalitabishal <kalitabishal@icloud.com>
2025-03-21 13:27:25 +0100

    added 2 Programs

---
+++ Programs/String_rev.java
@@ -0,0 +1,28 @@
+package Java.Programs;
+
+import java.util.Scanner;
+
+public class String_rev {
+    public static String reverse(String str) {
+        char [] chars = str.toCharArray();
+        int i = 0, j = chars.length - 1;
+
+        while (i < j) {
+            char temp = chars[i];
+            chars[i] = chars[j];
+            chars[j] = temp;
+
+            i++;
+            j--;
+        }
+        return new String(chars);
+    }
+    public static void main(String[] args) {
+       System.out.println("Enter a String");
+       Scanner stringScanner = new Scanner(System.in);
+        String original = stringScanner.next();
+        String reversed = reverse(original);
+        System.out.println("Original:" + original);
+        System.out.println("Reversed: " + reversed);
+    }
+}
